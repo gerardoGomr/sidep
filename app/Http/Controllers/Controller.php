1caddafcaad2d0e3a61460a89e45cfb748345ec6
Gerardo Gómez <gerardo.gomr@gmail.com>
2017-02-01 14:32:55 -0600

    Se migró a Laravel 5.3

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,16 +3,14 @@
 namespace Sidep\Http\Controllers;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Auth\Access\AuthorizesResources;
 use Sidep\Aplicacion\TransformadorMayusculas;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     /**
      * transformar request a mayusculas
